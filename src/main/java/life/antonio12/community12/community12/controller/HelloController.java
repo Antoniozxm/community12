8949564765c298e04898c2d1c3213881e1d214bb
swe1709509@xmu.edu.my <swe1709509@xmu.edu.my>
2019-06-03 01:00:11 +0800

    first commit

---
+++ src/main/java/life/antonio12/community12/community12/controller/HelloController.java
@@ -0,0 +1,18 @@
+package life.antonio12.community12.community12.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
